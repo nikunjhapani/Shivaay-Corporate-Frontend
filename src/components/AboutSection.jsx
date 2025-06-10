47a7efaba05bbcc2b2476748009d4e6bad5b852a
JabarsinghDev <jabarsingh.code@gmail.com>
2025-06-10 15:40:03 +0530

    Home page about images

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -4,43 +4,43 @@ import React from "react";
 import Image from "next/image";
 import Typography from "./ui/Typography";
 import Button from "./ui/Button";
+import { useQuery } from "@tanstack/react-query";
+import { postData } from "../utils/apiMethods";
+import api from "../utils/axios";
+import Link from "next/link";
+export const getAboutImages = async () => {
+  const res = await postData("/api/about/getAllApi");
+  return res?.data || [];
+};
 
 export default function About() {
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["about"],
+    queryFn: getAboutImages,
+  });
   return (
     <section className="about -type-1 layout-pt-md">
       <div className="container">
         <div className="about__images">
-          <div data-aos="zoom-in" data-aos-offset="0" data-aos-duration="1000">
-            <Image
-              src="/img/about/12/1.png"
-              alt="image"
-              className="rounded-16"
-              width={500}
-              height={500}
-            />
-          </div>
-
-          <div data-aos="zoom-in" data-aos-offset="0" data-aos-duration="1000">
-            <Image
-              src="/img/about/12/2.png"
-              alt="image"
-              className="rounded-16"
-              width={500}
-              height={500}
-            />
-          </div>
-
-          <div data-aos="zoom-in" data-aos-offset="0" data-aos-duration="1000">
-            <Image
-              src="/img/about/12/3.png"
-              alt="image"
-              className="rounded-16"
-              width={500}
-              height={500}
-            />
-          </div>
+          {data
+            ?.filter((item) => item.isActive)
+            .map((item, index) => (
+              <div
+                data-aos="zoom-in"
+                data-aos-offset="0"
+                data-aos-duration="1000"
+                key={item.id || index}
+              >
+                <Image
+                  src={`${api.defaults.baseURL}/${item?.aboutImage}`}
+                  alt="image"
+                  className="rounded-16"
+                  width={500}
+                  height={500}
+                />
+              </div>
+            ))}
         </div>
-
         <div className="about__content01 mt-40">
           <div className="row justify-center text-center">
             <div className="col-xl-8 col-lg-10">
@@ -66,9 +66,11 @@ export default function About() {
               </div>
 
               <div>
-                <a href="/our-journey.html">
-                  <Button icon className="mx-auto mt-30 md:mt-20">DISCOVER MORE</Button>
-                </a>
+                <Link href="/our-journey">
+                  <Button icon className="mx-auto mt-30 md:mt-20">
+                    DISCOVER MORE
+                  </Button>
+                </Link>
               </div>
             </div>
           </div>
