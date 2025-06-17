71cc7ce439b2c0be85f28eae9092177c23c102ee
Nens Patel <patelnens512@gmail.com>
2025-06-17 14:43:52 +0530

    Header changes

--- src/app/layout/Header.tsx
+++ src/app/layout/Header.tsx
@@ -63,10 +63,32 @@ export default function Header({ settings, setSidebarOpen, menu, submenuMap }) {
           <div className="header__right d-flex items-center h-full">
             <div className="line -vertical bg-white-10 h-full ml-90 mr-90 xl:d-none"></div>
 
-            <Link href="/contact-us">
-              <button className="button text-white mr-30 xl:d-none">
-                <i className="icon-phone text-20 text-white mr-15"></i>
-                CONTACT US
+            <Link href="https://lajo.in/" target="_blank">
+              <button className="button text-white mr-30 ">
+                <svg
+                  className="icon"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M20 21V19C20 17.9391 19.5786 16.9217 18.8284 16.1716C18.0783 15.4214 17.0609 15 16 15H8C6.93913 15 5.92172 15.4214 5.17157 16.1716C4.42143 16.9217 4 17.9391 4 19V21"
+                    stroke="#fff"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  ></path>
+                  <path
+                    d="M12 11C14.2091 11 16 9.20914 16 7C16 4.79086 14.2091 3 12 3C9.79086 3 8 4.79086 8 7C8 9.20914 9.79086 11 12 11Z"
+                    stroke="#fff"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  ></path>
+                </svg>
+                <span className="ps-2 xl:d-none">LOGIN</span>
               </button>
             </Link>
           </div>
