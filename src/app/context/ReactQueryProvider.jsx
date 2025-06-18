658d882e8a7c54618f1d251ac57a9200644d9576
Rahul Katariya <rahulk.setblue@gmail.com>
2025-06-18 10:22:06 +0530

    Update ReactQueryProvider.jsx

--- src/app/context/ReactQueryProvider.jsx
+++ src/app/context/ReactQueryProvider.jsx
@@ -4,7 +4,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
 
 export default function ReactQueryProvider({ children }) {
-  // Create QueryClient only once
   const [queryClient] = useState(() => new QueryClient());
 
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
